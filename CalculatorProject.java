d90cb095ba7339984b3d0833333cc0a598a9ff32
Pavan A C <pavanpavu64@gmail.com>
2020-01-31 13:57:48 +0530

    update

--- CalculatorProject.java
+++ CalculatorProject.java
@@ -6,26 +6,16 @@ public class CalculatorProject {
 		System.out.println("===========================");
 		System.out.println("Enter Two  Number for Basic Airthmetic opertaion");
 		Scanner scan = new Scanner(System.in);
-<<<<<<< HEAD
+
         int a = scan.nextInt();
         int b = scan.nextInt();
         System.out.println(a);
         System.out.println(b);
         pavan
-		
-=======
+
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		System.out.println(a);
 		System.out.println(b);
 		pow(a, b);
->>>>>>> 11f3dfc2a68c2d566e0207001e9245ed6ffb7a61
-	}
-
-	public static void pow(final int a, final int b)
-	{
-		final int p = (int) Math.pow((double)a, (double)b);
-		System.out.println("Result : "+p);
-	}
 }
-ACP
